6c821f9c47153a80911f9f6b60ecf3b15db70a13
Jordi Vilagut Treserra <Jordi Vilagut Treserra>
2017-02-17 04:51:17 +0100

    Implement navigation through layouts

--- app/src/main/java/com/jordivilagut/epurchase/Checkout.java
+++ app/src/main/java/com/jordivilagut/epurchase/Checkout.java
@@ -1,13 +1,27 @@
 package com.jordivilagut.epurchase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Checkout extends AppCompatActivity {
+
+    Button checkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        checkout = (Button)findViewById(R.id.checkoutBtn);
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent checkout = new Intent(Checkout.this, MainActivity.class);
+                startActivity(checkout);
+            }
+        });
     }
 }
--- app/src/main/java/com/jordivilagut/epurchase/MainActivity.java
+++ app/src/main/java/com/jordivilagut/epurchase/MainActivity.java
@@ -1,14 +1,38 @@
 package com.jordivilagut.epurchase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goToAddProduct;
+    Button goToCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToAddProduct = (Button)findViewById(R.id.addBtn);
+        goToAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAddProduct = new Intent(MainActivity.this, AddProduct.class);
+                startActivity(goToAddProduct);
+            }
+        });
+
+        goToCheckout = (Button)findViewById(R.id.checkoutBtn);
+        goToCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToCheckout = new Intent(MainActivity.this, Checkout.class);
+                startActivity(goToCheckout);
+            }
+        });
     }
 
     public static void main(String[] args) {
