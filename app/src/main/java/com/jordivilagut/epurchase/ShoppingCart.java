06cb1a832821849fbff871907833251d0659a0c7
Jordi Vilagut Treserra <Jordi Vilagut Treserra>
2017-02-18 20:56:00 +0100

    Align products in product list

--- app/src/main/java/com/jordivilagut/epurchase/ShoppingCart.java
+++ app/src/main/java/com/jordivilagut/epurchase/ShoppingCart.java
@@ -1,6 +1,8 @@
 package com.jordivilagut.epurchase;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -24,8 +26,19 @@ public class ShoppingCart implements Serializable {
 
     public String toString() {
         StringBuilder builder = new StringBuilder();
+        NumberFormat formatter = new DecimalFormat("#0.00");
+
         for (Map.Entry<String, Product> product : products.entrySet()) {
-            builder.append(product.getValue().getPrice() + " € " + product.getValue().getName() + "\n");
+            String price = formatter.format(product.getValue().getPrice());
+            String name = product.getValue().getName();
+            int emptyPositions = 14 - String.valueOf(price).length();
+            String space = "";
+
+            for (int i = 0; i < emptyPositions ; i++) {
+                space += "\u2007";
+            }
+
+            builder.append(space + price + " € " + name + "\n");
         }
         return builder.toString();
     }
